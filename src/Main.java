8706805644ea89a03c58bdeb7d74f144c0abc139
“IoanaBoros95” <boros.ioana95@gmail.com>
2021-03-29 22:14:15 +0300

    Tema-Lab9-Introducere in programare Java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello word!");
+        // 1.Printati "Hello" in consola, iar pe rand nou, printati numele vostru.
+        System.out.println("Hello");
+        System.out.println("Ioana");
+
+        // 2.Printati rezultatul sumei a doua numere(orice numere)
+
+        int x = 1;
+        int y = 2;
+        int sum = x+y;
+        System.out.println("Suma celor doua numere este:" +sum);
+
+        // 3.Printati rezultatul impartirii a doua numere. Atentie, daca numarul nu este intreg, sa afiseze si valorile dupa virgula.
+
+        int a = 10;
+        int b = 2;
+        int c = a/b;
+        System.out.println("Rezultatul impartirii celor doua numere este:" +c);
+
+        // 4.Printati rezultatul urmatoarelor operatiuni:
+
+        // a
+        int d = -5+8*6;
+        System.out.println("Rezultatul operatiei de la punctul a este:" +d);
+
+        //b
+        double e = (55+9) % 9;
+        System.out.println("Rezultatul operatiei de la punctul b este:" +e);
+
+        //c
+        double f = 20 + -3*5 / 8;
+        System.out.println("Rezultatul operatiei de la punctul c este:" +f);
+
+        //d
+        double g = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println("Rezultatul operatiei de la punctul d este:" +g);
     }
 }
