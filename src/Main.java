de91c12033e9b868993ad8f9feabede9d537eea0
“IoanaBoros95” <boros.ioana95@gmail.com>
2021-03-31 18:01:29 +0300

    Tema-Lab9-Final

--- src/Main.java
+++ src/Main.java
@@ -22,19 +22,19 @@ public class Main {
         // 4.Printati rezultatul urmatoarelor operatiuni:
 
         // a
-        int d = -5+8*6;
+        int d = (-5+(8*6));
         System.out.println("Rezultatul operatiei de la punctul a este:" +d);
 
         //b
-        double e = (55+9) % 9;
+        double e = ((55+9) % 9);
         System.out.println("Rezultatul operatiei de la punctul b este:" +e);
 
         //c
-        double f = 20 + -3*5 / 8;
+        double f = (20 + (-3*5 / 8f));
         System.out.println("Rezultatul operatiei de la punctul c este:" +f);
 
         //d
-        double g = 5 + 15 / 3 * 2 - 8 % 3;
+        double g = (5 + (15 / 3 * 2) - (8 % 3));
         System.out.println("Rezultatul operatiei de la punctul d este:" +g);
     }
 }
